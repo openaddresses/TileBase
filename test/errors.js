baf875257f17b89290eec65b6f58480d5b00d746
ingalls <nick@ingalls.ca>
2021-12-22 10:03:39 -0400

    Update lints & doc

--- test/errors.js
+++ test/errors.js
@@ -10,21 +10,21 @@ test('Errors', async (t) => {
         const tb = new TileBase('file://' + path.resolve(__dirname, './fixtures', 'min.tb'));
 
         try {
-            await tb.tile(1, 1, 1)
+            await tb.tile(1, 1, 1);
             t.fail();
         } catch (err) {
             t.deepEquals(err, new TBError(400, 'TileBase file is not open'));
         }
 
         try {
-            await tb.tilejson()
+            await tb.tilejson();
             t.fail();
         } catch (err) {
             t.deepEquals(err, new TBError(400, 'TileBase file is not open'));
         }
 
         try {
-            await tb.close()
+            await tb.close();
             t.fail();
         } catch (err) {
             t.deepEquals(err, new TBError(400, 'TileBase file is already closed'));
@@ -41,35 +41,35 @@ test('Errors', async (t) => {
         });
 
         try {
-            await tb.tile(13, 1, 1)
+            await tb.tile(13, 1, 1);
             t.fail();
         } catch (err) {
             t.deepEquals(err, new TBError(404, 'Zoom not supported'));
         }
 
         try {
-            await tb.tile(14, 2, 1)
+            await tb.tile(14, 2, 1);
             t.fail();
         } catch (err) {
             t.deepEquals(err, new TBError(404, 'X above range'));
         }
 
         try {
-            await tb.tile(14, 0, 1)
+            await tb.tile(14, 0, 1);
             t.fail();
         } catch (err) {
             t.deepEquals(err, new TBError(404, 'X below range'));
         }
 
         try {
-            await tb.tile(14, 1, 2)
+            await tb.tile(14, 1, 2);
             t.fail();
         } catch (err) {
             t.deepEquals(err, new TBError(404, 'Y above range'));
         }
 
         try {
-            await tb.tile(14, 1, 0)
+            await tb.tile(14, 1, 0);
             t.fail();
         } catch (err) {
             t.deepEquals(err, new TBError(404, 'Y below range'));
